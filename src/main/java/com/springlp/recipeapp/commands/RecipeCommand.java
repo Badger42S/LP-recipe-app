0bb4a554f4df380631e8b924e7a10664312c54b9
Badger42S <k3gbdia1@gmail.com>
2020-01-03 06:49:38 -0400

    update th template recipeform for unvalid data in fields

--- src/main/java/com/springlp/recipeapp/commands/RecipeCommand.java
+++ src/main/java/com/springlp/recipeapp/commands/RecipeCommand.java
@@ -1,20 +1,39 @@
 package com.springlp.recipeapp.commands;
 
 import com.springlp.recipeapp.domain.Difficulty;
+import org.hibernate.validator.constraints.URL;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
 public class RecipeCommand {
     private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String description;
+    @Min(1)
+    @Max(999)
     private Integer prepTime;
+    @Min(1)
+    @Max(999)
     private Integer cookTime;
+    @Min(1)
+    @Max(30)
     private Integer serving;
+
     private Set<IngredientCommand> ingredients=new HashSet<>();
+
     private String source;
+    @URL
     private String url;
+    @NotBlank
     private String directions;
+
     private Difficulty difficulty;
     private Set<CategoryCommand> category=new HashSet<>();
     private NotesCommand notes;
